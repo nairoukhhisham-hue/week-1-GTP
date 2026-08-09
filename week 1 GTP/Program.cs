3dbeabfcee256b9b364b5fcdb278ffb6f655eb34
Lenovo <Lenovo@DESKTOP-H7FMVK6>
2026-08-09 11:15:32 +0300

    Add project files.

---
+++ week 1 GTP/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_1_GTP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Hello World");
+
+            String x = "Hisham ";
+
+            int age1  = 21;
+
+            double courseDuration1 = 4.8;
+
+            String y =" omar " ;
+
+            int age2 = 22;
+
+            double courseDuration2 = 5.8;
+            Console.WriteLine("Traineers information ");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Traineer 1 : ");
+            Console.WriteLine("Name : "+ x);
+
+            Console.WriteLine("Age : "+ age1);
+
+            Console.WriteLine("Cours Duraion :" + courseDuration1);
+            Console.WriteLine("Traineer 2 : ");
+            Console.WriteLine("Name : " +y);
+
+            Console.WriteLine("Age : " + age2);
+
+            Console.WriteLine("Cours Duraion :" + courseDuration2);
+
+        }
+    }
+}
